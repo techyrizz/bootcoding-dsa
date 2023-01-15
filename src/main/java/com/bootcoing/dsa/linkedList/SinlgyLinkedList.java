3f899da769c09fc094a992cae34e8739755db91e
Rizwan Shaikh <rizwanshaikh.sr01@gmail.com>
2023-01-15 14:35:35 +0530

    Linked list

---
+++ src/main/java/com/bootcoing/dsa/linkedList/SinlgyLinkedList.java
@@ -0,0 +1,32 @@
+package com.bootcoing.dsa.linkedList;
+public class SinlgyLinkedList {
+    public static void main(String[] args) {
+        Node head = createLinkedList();
+        printLinkedList(head);
+    }
+        public static Node createLinkedList ()
+        {
+            Node firstNode = new Node(10);
+            Node secondNode = new Node(20);
+            Node thirdNode = new Node(30);
+            Node forthNode = new Node(40);
+
+            Node head = firstNode;
+            head.next = secondNode;
+            secondNode.next = thirdNode;
+            thirdNode.next = forthNode;
+            forthNode.next = null;
+            return head;
+        }
+
+        public static void printLinkedList (Node head)
+        {
+            Node temp = head;
+            while (temp != null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+
+
+}
